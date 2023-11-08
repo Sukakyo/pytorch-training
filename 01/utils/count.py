f475effe9138cb8a4a392ab2567210cf34663da0
Sukakyo <takahashi.great666t@gmail.com>
2023-11-08 13:06:52 +0900

    create count.py and function.py

---
+++ 01/utils/count.py
@@ -0,0 +1,11 @@
+from utils.function import function
+
+def count_word(s,character):
+  assert isinstance(s,str)
+  assert isinstance(character,str)
+  assert len(character) == 1
+  num = 0
+  for i in range(len(s)):
+    if s[i] == character:
+      num = function.add(num,1)
+  return num
---
+++ 01/utils/function.py
@@ -0,0 +1,6 @@
+
+print("function.py is imported")
+
+def add(a, b):
+  ret = a + b
+  return ret
